8e66419c04449268cf19c132fab47779aff0fdd2
Morningxxx <spechles@spechles.com>
2016-02-26 11:09:24 +0800

    1st improvement

--- DrawingBoardWithBezierPath/JCGView.m
+++ DrawingBoardWithBezierPath/JCGView.m
@@ -10,10 +10,17 @@
 
 @interface JCGView()
 
+/*point array or dic for drawing*/
 @property (nonatomic, strong) NSMutableArray<NSDictionary*>* pointAll;
 @property (nonatomic, strong) NSMutableArray* pointCur;
+@property (nonatomic, strong) NSMutableArray<NSDictionary*>* redoArr;
+
+/*controller*/
 @property (weak,nonatomic) UISegmentedControl* penEraserChose;
-@property (strong,nonatomic) UIColor* fillColor;
+@property (weak,nonatomic) UIButton* redoBtn;
+
+/*drawing property*/
+@property (strong,nonatomic) UIColor* strokeColor;
 @property (assign,nonatomic) CGFloat penWidth;
 @property (assign,nonatomic) CGFloat eraserWidth;
 
@@ -31,24 +38,55 @@
 
 -(instancetype)initWithFrame:(CGRect)frame{
     if(self = [super initWithFrame:frame]){
+        /*creat Clear Button*/
         UIButton* clearBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        clearBtn.frame = CGRectMake(20, 30, 60, 20);
+        clearBtn.frame = CGRectMake(0, 30, 60, 20);
         [clearBtn setTitle:@"Clear" forState:UIControlStateNormal];
         [clearBtn addTarget:self action:@selector(clearBoard) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:clearBtn];
+        
+        /*creat Undo Button*/
+        UIButton* undoBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        undoBtn.frame = CGRectMake(60, 30, 50, 20);
+        [undoBtn setTitle:@"Undo" forState:UIControlStateNormal];
+        [undoBtn addTarget:self action:@selector(undo) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:undoBtn];
+        
+        /*creat Save Button*/
+        UIButton* saveBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        saveBtn.frame = CGRectMake(self.bounds.size.width-50, 30, 50, 20);
+        [saveBtn setTitle:@"Save" forState:UIControlStateNormal];
+        [saveBtn addTarget:self action:@selector(saveAsImage) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:saveBtn];
+        
+        /*creat Redo Button*/
+        UIButton* redoBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        redoBtn.frame = CGRectMake(self.bounds.size.width-50-50, 30, 50, 20);
+        [redoBtn setTitle:@"Redo" forState:UIControlStateNormal];
+        [redoBtn addTarget:self action:@selector(redo) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:redoBtn];
+        self.redoBtn = redoBtn;
+        self.redoBtn.enabled = self.redoArr.count;
+
+        
+        /*creat pen-or-eraser choser*/
         UISegmentedControl* panErazerChose = [[UISegmentedControl alloc]initWithFrame:CGRectMake(self.bounds.size.width/2-50, 30, 100, 20)];
         self.penEraserChose= panErazerChose;
-        [panErazerChose insertSegmentWithTitle:@"pan" atIndex:0 animated:NO];
+        [panErazerChose insertSegmentWithTitle:@"pen" atIndex:0 animated:NO];
         [panErazerChose insertSegmentWithTitle:@"eraser" atIndex:1 animated:NO];
         panErazerChose.selectedSegmentIndex = 0;
         [self addSubview:panErazerChose];
-        self.fillColor = [UIColor blackColor];
+        
+        /*set defult drawing property*/
+        self.strokeColor = [UIColor blackColor];
         self.penWidth = 3;
         self.eraserWidth = 10;
     }
     return self;
 }
 
+#pragma mark - Lazy Loading
+/*lazy loading*/
 -(NSMutableArray*)pointAll{
     if (!_pointAll)
         _pointAll = [NSMutableArray array];
@@ -62,81 +100,142 @@
     return _pointCur;
 }
 
+-(NSMutableArray<NSDictionary *> *)redoArr{
+    if (!_redoArr) {
+        _redoArr = [NSMutableArray array];
+    }
+    return _redoArr;
+}
 
 
+#pragma mark - userAction
+/*undo last drawing*/
+-(void)undo{
+    if ([self.pointAll lastObject]) {
+        [self.redoArr addObject:[self.pointAll lastObject]];
+    }
+    [self.pointAll removeLastObject];
+    [self setNeedsDisplay];
+    self.redoBtn.enabled = self.redoArr.count;
+
+}
+
+/*redo last undo or clear*/
+-(void)redo{
+    if(_redoArr.count){
+        if (!_pointAll) {
+            self.pointAll = _redoArr;
+            [self setNeedsDisplay];
+            _redoArr = nil;
+
+        }else{
+            [self.pointAll addObject:[self.redoArr lastObject]];
+            [self.redoArr removeLastObject];
+            [self setNeedsDisplay];
+        }
+    }
+    self.redoBtn.enabled = self.redoArr.count;
+
+}
+
+/*clear board*/
+-(void)clearBoard{
+    self.redoArr = self.pointAll;
+    self.pointCur = nil;
+    self.pointAll = nil;
+    [self setNeedsDisplay];
+    self.redoBtn.enabled = self.redoArr.count;
+
+}
+
+-(void)saveAsImage{
+    UIGraphicsBeginImageContext(self.bounds.size);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    [self.layer renderInContext:ctx];
+    UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
+    
+    UIGraphicsEndImageContext();
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+}
+
+
+#pragma mark - Touch Events
+/*touch events*/
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     self.pointCur = nil;
     self.penEraserChose.userInteractionEnabled = NO;
     [self.pointCur addObject:[NSValue valueWithCGPoint:[[touches anyObject] locationInView:self]]];
 }
-
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 
     [self.pointCur addObject:[NSValue valueWithCGPoint:[[touches anyObject] locationInView:self]]];
     [self setNeedsDisplay];
 }
-
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     
     [self.pointCur addObject:[NSValue valueWithCGPoint:[[touches anyObject] locationInView:self]]];
     
-    NSDictionary* path = @{_penEraserChose.selectedSegmentIndex ? @"eraser":@"pen" : self.pointCur};
-    [self.pointAll addObject:path];
+    if (self.pointCur.count>2) {
+        NSDictionary* path = @{_penEraserChose.selectedSegmentIndex ? @"eraser":@"pen" : self.pointCur};
+        [self.pointAll addObject:path];
+    }
     self.penEraserChose.userInteractionEnabled = YES;
-
-}
-
--(void)clearBoard{
     self.pointCur = nil;
-    self.pointAll = nil;
-    [self setNeedsDisplay];
+    self.redoArr = nil;
+    self.redoBtn.enabled = self.redoArr.count;
 }
 
 
+#pragma mark - Drawing
+/*drawing method*/
 -(void)drawRect:(CGRect)rect{
     if (_pointAll) {
         for (NSDictionary* pointDic in _pointAll) {
-            UIBezierPath* path = [[UIBezierPath alloc]init];
-            NSArray* pointArr = pointDic[@"pen"];
-            [self.fillColor setStroke];
-            [path moveToPoint:[pointArr.firstObject CGPointValue]];
-            for (NSValue* point in pointArr) {
-                CGPoint p = [point CGPointValue];
-                [path addLineToPoint:p];
-            }
-            [path setLineWidth:self.penWidth];
-            [path stroke];
-            UIBezierPath* path1 = [[UIBezierPath alloc]init];
-            pointArr = pointDic[@"eraser"];
-            [self.backgroundColor setStroke];
-            [path1 moveToPoint:[pointArr.firstObject CGPointValue]];
-            for (NSValue* point in pointArr) {
-                CGPoint p = [point CGPointValue];
-                [path1 addLineToPoint:p];
-            }
-            [path1 setLineWidth:self.eraserWidth];
-            [path1 stroke];
             
+            NSArray* pointArr = pointDic[@"pen"];
+            [self drawPanActionWithPointArray:pointArr];
+            
+            pointArr = pointDic[@"eraser"];
+            [self drawEraserActionWithPointArray:pointArr];
         }
     }
 
-    if (_pointCur.count > 1) {
+    if (_pointCur.count > 2) {
         if (!self.penEraserChose.selectedSegmentIndex) {
-            [self.fillColor setStroke];
+            [self drawPanActionWithPointArray:_pointCur];
         }
         else{
-            [self.backgroundColor setStroke];
+            [self drawEraserActionWithPointArray:_pointCur];
         }
-        UIBezierPath* path = [[UIBezierPath alloc]init];
-        [path moveToPoint:[_pointCur.firstObject CGPointValue]];
-        for (NSValue* point in _pointCur) {
-            CGPoint p = [point CGPointValue];
-            [path addLineToPoint:p];
-        }
-        [path setLineWidth:_penEraserChose.selectedSegmentIndex ? self.eraserWidth:self.penWidth];
-        [path stroke];
     }
-    
+}
+
+-(void)drawPanActionWithPointArray:(NSArray*)pointArr{
+    [self.strokeColor setStroke];
+    UIBezierPath* path = [[UIBezierPath alloc]init];
+    [path moveToPoint:[pointArr.firstObject CGPointValue]];
+    for (NSValue* point in pointArr) {
+        CGPoint p = [point CGPointValue];
+        [path addLineToPoint:p];
+    }
+    [path setLineWidth:self.penWidth];
+    [path stroke];
+}
+
+-(void)drawEraserActionWithPointArray:(NSArray*)pointArr{
+    [self.backgroundColor setStroke];
+    UIBezierPath* path = [[UIBezierPath alloc]init];
+    [path moveToPoint:[pointArr.firstObject CGPointValue]];
+    for (NSValue* point in pointArr) {
+        CGPoint p = [point CGPointValue];
+        [path addLineToPoint:p];
+    }
+    [path setLineWidth:self.eraserWidth];
+    [path stroke];
 }
 
 @end
